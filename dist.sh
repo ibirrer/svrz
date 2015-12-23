86ccec30f38c677cec3c87a83063212849284dde
Iwan Birrer <ibirrer@gmail.com>
2015-12-23 20:55:55 +0100

    fixed dist script again..

--- dist.sh
+++ dist.sh
@@ -8,7 +8,7 @@ npm install
 npm run make
 sed -i '/build/d' .gitignore
 git add . && git commit -m "prepare dist"
-git subtree split --prefix dist -b gh-pages
+git subtree split --prefix build -b gh-pages
 git push -f origin gh-pages:gh-pages
 git branch -D gh-pages
 git checkout master
