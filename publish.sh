0d413c70874aa57f6f6bf5e6fbb5aa3ed803edd1
Iwan Birrer <ibirrer@gmail.com>
2015-12-25 13:13:24 +0100

    improve dev mode (watch)

--- publish.sh
+++ publish.sh
@@ -5,10 +5,10 @@ set -e
 git checkout -b dist
 git clean -xdf
 npm install
-npm run make
-sed -i '/build/d' .gitignore
+npm run dist
+sed -i '/dist/d' .gitignore
 git add . && git commit -m "prepare dist"
-git subtree split --prefix build -b gh-pages
+git subtree split --prefix dist -b gh-pages
 git push -f origin gh-pages:gh-pages
 git branch -D gh-pages
 git checkout master
